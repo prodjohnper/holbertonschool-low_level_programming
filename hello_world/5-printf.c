3c4fc26d2fe3ceb464eccd18994c70fe02ca3112
prodjohnper <jonathanperez9743@gmail.com>
2023-10-12 14:20:56 -0400

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ *main - Print exact phrase followed by a new line
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
