c7666ead2522b02d027564dd64d53a3bcd3047ee
prodjohnper <jonathanperez9743@gmail.com>
2023-10-12 14:59:18 -0400

    %lu\n

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Print the size of various types on the computer
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu\n", sizeof(long long int));
+	printf("%lu\n", sizeof(float);
+
+return (0);
+}
