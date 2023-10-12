bfdbe7d02694a0eb7cf349eff0948da3ab733a53
prodjohnper <jonathanperez9743@gmail.com>
2023-10-12 15:01:16 -0400

    %lu\n

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	printf("%lu\n", sizeof(char));
-	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(long int));
-	printf("%lu\n", sizeof(long long int));
-	printf("%lu\n", sizeof(float);
+	printf("%zu\n", sizeof(char));
+	printf("%zu\n", sizeof(int));
+	printf("%zu\n", sizeof(long int));
+	printf("%zu\n", sizeof(long long int));
+	printf("%zu\n", sizeof(float);
 
 return (0);
 }
