f6cc401bd69673942806731897ebefa12a6cbbe5
prodjohnper <jonathanperez9743@gmail.com>
2023-10-16 15:11:41 -0400

    Combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print all posible single-digit
+ * number combinations
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int comb;
+
+	for (comb = 0; comb < 10; comb++)
+	{
+		putchar(comb + '0');
+		if (comb < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
