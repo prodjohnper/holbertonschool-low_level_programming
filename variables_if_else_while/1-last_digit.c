21f369883cf791e2f3661b82ca119099f933f69e
prodjohnper <jonathanperez9743@gmail.com>
2023-10-16 11:01:37 -0400

    Last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ int main(void)
 
 	{
 
-	if (n > 0)
+	if (y > 0)
 	{
 		printf(" and is greater than 5\n");
 	}
-	else if (n < 0)
+	else if (y < 0)
 	{
 		printf(" and is less than 6 and not 0\n");
 	}
