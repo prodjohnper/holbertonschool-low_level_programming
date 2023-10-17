5f97fc1799a1c952011334492189e8ae3f959327
prodjohnper <jonathanperez9743@gmail.com>
2023-10-17 10:50:29 -0400

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Print the alphabet in
+ * lowercase, followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+	return ;
+}
