b5e6e3c0e5a9a914a1bf2fea73163a22df5fe4e5
prodjohnper <jonathanperez9743@gmail.com>
2023-10-18 10:04:36 -0400

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** Compute the absolute value
+ * of an integer
+ *
+ * Return: Success 1, negative -1 otherwise 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else if (n > 0)
+	{
+		return (+n);
+	}
+	else
+	{
+		return (0);
+	}
+}
