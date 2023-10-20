e4edd2a41bd27091e467fdeb07039cefbf0cbf4d
prodjohnper <jonathanperez9743@gmail.com>
2023-10-20 15:41:57 -0400

    6-print_line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight 
+ * print_line - Draws a straight
  * line in the terminal
+ *
+ * @n: variable
  */
 
 void print_line(int n)
 {
 	int l;
 
-	if(n > 0)
+	if (n > 0)
 	{
 		for (l = 0; l < n; l++)
 			_putchar('_');
