fae4588a0f291be58f8bc6afdd8566fc2bad1982
prodjohnper <jonathanperez9743@gmail.com>
2023-10-20 15:40:12 -0400

    6-print_line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight 
+ * line in the terminal
+ */
+
+void print_line(int n)
+{
+	int l;
+
+	if(n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+
+}
