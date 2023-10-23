2d7580ae8a3d3b9193f8bb971fd21ff085a20957
prodjohnper <jonathanperez9743@gmail.com>
2023-10-23 14:33:50 -0400

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -17,9 +17,9 @@ void print_rev(char *s)
 	}
 
 	for (index = index - 1; s[index] != '\0'; index--)
-		{
-			_putchar(s[index]);
-		}
+	{
+		_putchar(s[index]);
+	}
 
-		_putchar('\n');
+	_putchar('\n');
 }
