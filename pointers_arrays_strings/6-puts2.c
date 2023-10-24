e02b9c2005927d792d145060ed56cbec0031adb8
prodjohnper <jonathanperez9743@gmail.com>
2023-10-24 09:30:12 -0400

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other
+ * character of a string
+ *
+ * @str: variable
+ */
+
+void puts2(char *str)
+{
+
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
+}
