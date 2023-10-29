fca5c9f7715c7bd4d5429994529b9430ceeaf757
prodjohnper <jonathanperez9743@gmail.com>
2023-10-29 19:00:38 -0400

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the
- * natural square root of a number
+ * _sqrt_recursion - returns the natural square root of a number
  *
- * @n: variable
  * @r: variable
+ * @n: variable
  *
- * Return: natural square root of a
- * number or -1 otherwise.
+ * Return: natural square root or -1 otherwise.
  */
 
 int square(int r, int n);
-
 int _sqrt_recursion(int n)
 {
 	return (square(0, n));
 }
 
 /**
- * square - returns the square
- * root of a number
+ * square - function created
+ *
+ * @r: variable
+ * @n: variable
+ *
+ * Return: natural square root
  */
 
 int square(int r, int n)
