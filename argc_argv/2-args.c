9a17e4fb92b89767e05ec3b21468b63c70fcfd1f
prodjohnper <jonathanperez9743@gmail.com>
2023-10-31 14:10:18 -0400

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints
+ * it name, followed by a new
+ * line
+ *
+ * @argc: Argument count.
+ * @argv: Arguments.
+ *
+ * Return: 0 Success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
