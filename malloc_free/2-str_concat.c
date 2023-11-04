6f2b93cc11be2b28f334c24c162e2d33e404b45b
prodjohnper <jonathanperez9743@gmail.com>
2023-11-04 17:24:20 -0400

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
-
 /**
  * *str_concat - Function that
  * concatenates two strings.
@@ -17,51 +14,35 @@
 char *str_concat(char *s1, char *s2)
 {
 	/* Create variables */
-
-	int s1_len, s2_len, size, i = 0;
-	char *concat;
+	int s1_len, s2_len, total_len;
+	char *result;
 
 	/* Give variables a value */
-
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
-	size = s1_len + s2_len;
+	total_len = s1_len + s2_len;
 
-	concat = malloc((size + 1) * sizeof(char));
+	result = malloc(total_len + 1);
 
-	/* If statements */
-
-	if (concat == NULL)
-	{
-		return (NULL);
-	}
-
+	/* Cases */
 	if (s1 == NULL)
 	{
-		s1_len = 0;
+		s1 = "";
 	}
 
 	if (s2 == NULL)
 	{
-		s2_len = 0;
+		s2 = "";
 	}
 
-	/* Print concatenated strings */
-
-	while (*s1 != '\0')
+	if (result == NULL)
 	{
-		concat[i] = *s1;
-		s1++;
-		i++;
+		return (NULL);
 	}
 
-	while (*s2 != '\0')
-	{
-		concat[i] = *s2;
-		s2++;
-		i++;
-	}
-	
-	concat[i] = '\0';
-	return (concat);
+	/* Concatenate and print strings */
+	strcpy(result, s1);
+	strcat(result, s2);
+
+	return (result);
 }
