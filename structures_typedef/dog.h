c7f1128bfd5c929ff6ffa077e32aa20cbfd96dc8
prodjohnper <jonathanperez9743@gmail.com>
2023-11-10 23:43:46 -0400

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,14 +16,9 @@ float age;
 char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/**
- * dog_t - Typedef
- * for struct dog
- */
-
-typedef struct dog dog_t;
 
 #endif
