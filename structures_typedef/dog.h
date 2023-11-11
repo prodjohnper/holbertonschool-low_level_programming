79f91610304b0fc2f2e0f8f4138b94a74ad87314
prodjohnper <jonathanperez9743@gmail.com>
2023-11-10 21:41:53 -0400

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Info about dog.
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: Owner's name
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
