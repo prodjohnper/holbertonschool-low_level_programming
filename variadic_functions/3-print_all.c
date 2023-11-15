cc0abc820fef582899404f45ad594aed32f67ece
prodjohnper <jonathanperez9743@gmail.com>
2023-11-15 09:53:16 -0400

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -37,7 +37,7 @@ void print_all(const char * const format, ...)
 				printf("%s", s);
 				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' 
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
 					|| format[i] == 's') && format[i + 1] != '\0')
 		{
 			printf(", ");
